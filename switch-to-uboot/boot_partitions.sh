0efb088d6b259b5b04e9371daad522d9a43ff44a
Andrey Danin <danindrey@mail.ru>
2013-05-12 23:09:48 +0400

    Experimental: put kernel on lnx partition
    sos partition isn't enough for kernel and initrd in some cases
    (for example ubuntu 13.04)

--- switch-to-uboot/boot_partitions.sh
+++ switch-to-uboot/boot_partitions.sh
@@ -22,33 +22,61 @@ dump_boot_partitions()
     dump_partition lnx "${device}p2" || (echo No boot image in LNX partition; rm -rf lnx; exit 1)
 }
 
+_mount()
+{
+    device=$1
+    mnt_point=$2
+
+    mkdir -p ${mnt_point}/sos || return 1
+    mkdir -p ${mnt_point}/lnx || return 1
+
+    res=0
+    mount ${device}p1 ${mnt_point}/sos || return 1
+    mount ${device}p2 ${mnt_point}/lnx || (umount ${mnt_point}/sos; res=1)
+
+    return $res
+}
+
+_umount()
+{
+    mnt_point=$1
+    umount ${mnt_point}/sos
+    umount ${mnt_point}/lnx
+}
 
 configure_uboot()
 {
-    device="${1}p1"
+    device="${1}"
  
     mkimage -n MyRamDisk -A arm -O linux -T ramdisk -C gzip -d lnx/initrd.img lnx/initrd-uboot
     
-    mkfs.ext2 -F "$device" >/dev/null || exit 1
-    mkdir lnx/mnt
-    mount "$device" lnx/mnt || exit 1
-    mkdir lnx/mnt/boot || (umount lnx/mnt; exit 1)
-    cp lnx/zImage lnx/mnt/boot/zImage || (umount lnx/mnt; exit 1)
-    cp lnx/initrd-uboot lnx/mnt/boot/initrd-uboot || (umount lnx/mnt; exit 1)
+    mnt_point=lnx/mnt
+    mkdir $mnt_point
+
+    mkfs.ext2 -F "${device}p1" >/dev/null || exit 1
+    mkfs.ext2 -F "${device}p2" >/dev/null || exit 1
+
+    _mount "$device" $mnt_point || exit 1
+
+    mkdir ${mnt_point}/sos/boot || (_umount lnx/mnt; exit 1)
+    mkdir ${mnt_point}/lnx/boot || (_umount lnx/mnt; exit 1)
+
+    cp lnx/zImage ${mnt_point}/lnx/boot/zImage || (umount lnx/mnt; exit 1)
+    cp lnx/initrd-uboot ${mnt_point}/lnx/boot/initrd-uboot || (umount lnx/mnt; exit 1)
     
     cmdline=$(cat lnx/bootimg.cfg | grep cmdline | sed "s/cmdline = //")
 
-    cfg="lnx/mnt/boot/boot.cmd"
+    cfg="${mnt_point}/sos/boot/boot.cmd"
     echo "echo === boot.scr: loading from SOS partition ===
 mmc dev 0
 setenv bootargs '$cmdline'
-ext2load mmc 0 0x1000000 /boot/zImage
-ext2load mmc 0 0x2200000 /boot/initrd-uboot
+ext2load mmc 0:1 0x1000000 /boot/zImage
+ext2load mmc 0:1 0x2200000 /boot/initrd-uboot
 bootz 0x1000000 0x2200000" > $cfg
 
-    mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "myscript" -d "$cfg" lnx/mnt/boot/boot.scr
+    mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "myscript" -d "$cfg" ${mnt_point}/sos/boot/boot.scr
 
-    umount lnx/mnt
+    _umount $mnt_point
 }
 
 
