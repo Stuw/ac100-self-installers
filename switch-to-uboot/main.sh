dd2d646cc785281a1df2b50302b0318050990d12
Andrey Danin <danindrey@mail.ru>
2015-02-27 18:06:08 +0300

    Rework logic in case of absent NV partition table.

--- switch-to-uboot/main.sh
+++ switch-to-uboot/main.sh
@@ -7,38 +7,65 @@ error()
 	exit 1
 }
 
+
+detect_target() {
+	for dev in /dev/mmcblk*boot0; do
+		[ -e "$dev" ] || continue
+		echo "${dev:0:12}"
+		return 0
+	done
+
+	return 1
+}
+
 CONFIG_FILE="current.cfg"
 
 clear
 
+KEEP_PT="n"
+USE_BOOT_PARTITIONS="n"
+
 echo "Analyzing NV partition table..."
-sh ./dump_part_table.sh "$CONFIG_FILE" || error Failed to dump current partition table
+sh ./dump_part_table.sh "$CONFIG_FILE"
+if [ $? -ne 0 ]; then
+	echo -e "\nFailed to dump current partition table. It is damaged or absent."
+	echo -e "\nIf you want to install u-boot anyway say 'y'."
+	read -p "Continue (y/n) ? " CONTINUE_WO_PT
+	if [ x"$CONTINUE_WO_PT" != "xy" ]; then
+		exit
+	fi
 
-device=$(head -n1 "$CONFIG_FILE" | sed 's/#Generated_by_script_from_device_//g')
-echo "Boot device: $device"
+	dev="$(detect_target)"
+	[ -z "$dev" ] && error "Target device was not detected"
+	echo "Boot device: $device"
+else
+	device=$(head -n1 "$CONFIG_FILE" | sed 's/#Generated_by_script_from_device_//g')
+	echo "Boot device: $device"
 
-echo -e "\nPartition table:"
-sh ./show_partitions.sh "$CONFIG_FILE"
-echo -en "\n"
-read -p "Keep this partition table (y/n) ? " KEEP_PT
-if [ x"$KEEP_PT" != "xy" ] && [ x"$KEEP_PT" != "xn" ]; then
-	error Incorrect answer
-fi
+	echo -e "\nPartition table:"
+	sh ./show_partitions.sh "$CONFIG_FILE"
+	echo -en "\n"
+	read -p "Keep this partition table (y/n) ? " KEEP_PT
+	if [ x"$KEEP_PT" != "xy" ] && [ x"$KEEP_PT" != "xn" ]; then
+		error Incorrect answer
+	fi
 
-USE_BOOT_PARTITIONS="$KEEP_PT"
-if [ x"$KEEP_PT" == "xy" ]; then
-	echo -en "\n\n"
-	sh ./boot_partitions.sh --dump $device
-	res="$?"
-	if [ "$res" -ne "0" ]; then
-		echo -e "\nCan't find any boot image. You must configure u-boot yourself later."
-		read -p "Continue (y/n) ? "
-		if [ x"$REPLY" != "xy" ]; then
-		    exit
+	USE_BOOT_PARTITIONS="$KEEP_PT"
+	if [ x"$KEEP_PT" == "xy" ]; then
+		echo -en "\n\n"
+		sh ./boot_partitions.sh --dump $device
+		res="$?"
+		if [ "$res" -ne "0" ]; then
+			echo -e "\nCan't find any boot image. You must configure u-boot yourself later."
+			read -p "Continue (y/n) ? "
+			if [ x"$REPLY" != "xy" ]; then
+			    exit
+			fi
+			USE_BOOT_PARTITIONS="n"
 		fi
-		USE_BOOT_PARTITIONS="n"
 	fi
 fi
+
 
 echo -e "\nReady to install u-boot. Old bootloader will be removed."
 if [ x"$KEEP_PT" == "xy" ]; then
