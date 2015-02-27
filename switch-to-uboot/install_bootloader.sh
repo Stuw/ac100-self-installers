5428f7c0d6966223467cd235240f629592b58592
Andrey Danin <danindrey@mail.ru>
2015-02-27 16:15:38 +0300

    Fix install_bootloader.sh to use cbootimage from nVidia.

--- switch-to-uboot/install_bootloader.sh
+++ switch-to-uboot/install_bootloader.sh
@@ -12,23 +12,25 @@ install_bootloader()
 
 	bct_dev="${device}boot0"
 	echo "Dump configuration..."
-	dd if="$bct_dev" of="$bct" bs=4080 count=1 || (echo "Failed to copy bct to file"; exit 1)
-	bct_dump "$bct" > "$bct_config"
+	dd if="$bct_dev" of="$bct" bs=4080 count=1
 	if [ $? -ne "0" ]; then
-		echo "Failed to dump BCT config."
+   		echo "Failed to copy bct to file"
 		return 1
 	fi
 
-	echo "BootLoader = ${bootloader},0x00108000,0x00108000,Complete;" >> $bct_config
+	echo "Version       = 0x00020001;
+Bctcopy       = 1;
+Bctfile       = ${bct};
+BootLoader = ${bootloader},0x00108000,0x00108000,Complete;" >> $bct_config
 
-	echo 0 > "/sys/block/$(basename ${device})boot0/force_ro"
-	echo 0 > "/sys/block/$(basename ${device})boot1/force_ro"
-
-	cbootimage -d "$bct_config" "${new_bct}"
+	cbootimage --debug "$bct_config" "${new_bct}"
 	if [ $? -ne "0" ]; then
 		echo "Failed to gen new BCT."
 		return 1
 	fi
+
+	echo 0 > "/sys/block/$(basename ${device})boot0/force_ro"
+	echo 0 > "/sys/block/$(basename ${device})boot1/force_ro"
 
 	dd if=/dev/zero of=${device}boot0 2>/dev/null
 	dd if=/dev/zero of=${device}boot1 2>/dev/null
